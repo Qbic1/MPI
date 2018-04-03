864b8d838fbf7c50765bc6ff1278077e9a7e4c42
Qbic1 <timgabbasov@mail.ru>
2018-04-04 00:12:35 +0300

    fix errors

--- MPI6.cpp
+++ MPI6.cpp
@@ -1,45 +1,62 @@
 #include "stdafx.h"
 #include "time.h"
+#include "mpi.h"
 
-/*int main(int argc, char* argv[]) {
-	int x[8][8];
+#define LENGHT 8
+
+int main(int argc, char* argv[]) {
+	int x[8][LENGHT];
 	int ProcRank, ProcNum, N = 8;
+
 	int resultMin;
 	int resultMax;
-
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	int elements_per_proc = N * N / ProcNum;
+	int elements_per_proc = N * LENGHT / ProcNum;
 	int *subarr = new int[elements_per_proc];
 
-	int localmax;
-	int localmin;
+	int localmax, minmax;
+	int localmin, maxmin;
+
 	srand(time(0));
 	if (ProcRank == 0) {
 		for (int i = 0; i < N; i++)
 		{
-			for (int j = 0; j < N; j++) {
+			for (int j = 0; j < LENGHT; j++) {
 				x[i][j] = rand() % 100;
 				printf(" %d ", x[i][j]);
 			}
 			printf("\n");
 		}
 	}
+
 	MPI_Scatter(x, elements_per_proc, MPI_INT,
 		subarr, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
 
-	localmax = 0;
-	for (int i = 0; i < elements_per_proc; i++)
-		if (subarr[i] > localmax) localmax = subarr[i];
-	localmin = 100;
-	for (int i = 0; i < elements_per_proc; i++)
-		if (subarr[i] < localmin) localmin = subarr[i];
+	maxmin = 0;
+	minmax = 100;
+	for (int i = 0; i < elements_per_proc / LENGHT; i++)
+	{
+		localmin = 100;
+		localmax = 0;
+		for (int j = i*LENGHT; j < (i + 1)*LENGHT; j++)
+		{
+			if (subarr[j] < localmin) localmin = subarr[j];
+			if (subarr[j] > localmax) localmax = subarr[j];
+		}
+		if (localmin > maxmin)
+			maxmin = localmin;
+		if (localmax < minmax)
+			minmax = localmax;
+	}
 
-	MPI_Reduce(&localmin, &resultMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&localmax, &resultMax, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&minmax, &resultMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&maxmin, &resultMax, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+
 	if (ProcRank == 0)
-		printf("\nmin = %d\nmax = %d", resultMin, resultMax);
+		printf("\nminmax = %d\nmaxmin = %d", resultMin, resultMax);
+
 	MPI_Finalize();
 	return 0;
-}*/
+}
