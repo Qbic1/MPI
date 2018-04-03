6effcd5e7f00e23db5d4e3d87ca7209d1e49d718
Qbic1 <timgabbasov@mail.ru>
2018-04-03 23:11:37 +0300

    fix errors

--- MPI5.cpp
+++ MPI5.cpp
@@ -1,12 +1,11 @@
 #include "stdafx.h"
 #include "time.h"
+#include "mpi.h"
 
-/*int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 	int x[8];
 	int y[8];
 	int ProcRank, ProcNum, N = 8;
-	int resultMin;
-	int resultMax;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
@@ -14,7 +13,6 @@
 	int elements_per_proc = N / ProcNum;
 	int *subarr1 = new int[elements_per_proc];
 	int *subarr2 = new int[elements_per_proc];
-
 	srand(time(0));
 	if (ProcRank == 0) {
 		for (int i = 0; i < N; i++)
@@ -30,19 +28,29 @@
 		}
 		printf("\n");
 	}
-
 	MPI_Scatter(x, elements_per_proc, MPI_INT,
 		subarr1, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Scatter(y, elements_per_proc, MPI_INT,
 		subarr2, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
 
+	int sum = 0;
 	for (int i = 0; i < elements_per_proc; i++)
-		subarr1[i] *= subarr2[i];
+		sum+=subarr1[i] * subarr2[i];
 
-	MPI_Gather(subarr1, elements_per_proc, MPI_INT, x, elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
-	if (ProcRank == 0)
-		for (int i = 0; i < N; i++)
-			printf("%d ", x[i]);
+	if (ProcRank != 0)
+	{
+		MPI_Send(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	else
+	{
+		int temp = 0;
+		for (int i = 1; i < ProcNum; i++)
+		{
+			MPI_Recv(&temp, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			sum += temp;
+		}
+		printf("result = %d\n", sum);
+	}
 	MPI_Finalize();
 	return 0;
-}*/
+}
